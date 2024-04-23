751d00a5270badcfb1be06649f9b7971064dd3cf
SpaceMasterCarlsen <96848056+SpaceMasterCarlsen@users.noreply.github.com>
2024-04-23 14:29:28 +0200

    cat and dog on the roll

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog atlas = new Dog();
+        atlas.makeSound();
+        Cat crazy = new Cat();
+        crazy.makeSound();
+    }
+}
