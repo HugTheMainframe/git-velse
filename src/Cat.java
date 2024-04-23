ea429d34591104940a11c7bd3602dc583c302001
SpaceMasterCarlsen <96848056+SpaceMasterCarlsen@users.noreply.github.com>
2024-04-23 14:26:24 +0200

    5.

--- src/Cat.java
+++ src/Cat.java
@@ -2,6 +2,11 @@ public class Cat extends Animal{
 
     @Override
     public void makeSound() {
-        System.out.println("wof");
+        System.out.println("meow");
+    }
+
+    @Override
+    public boolean eat(String catFood){
+        return true;
     }
 }
--- src/Dog.java
+++ src/Dog.java
@@ -2,6 +2,11 @@ public class Dog extends Animal{
 
     @Override
     public void makeSound() {
-        System.out.println("Meow");
+        System.out.println("wof");
+    }
+
+    @Override
+    public boolean eat(String dogFood){
+        return true;
     }
 }
